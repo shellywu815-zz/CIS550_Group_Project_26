b750d0a6eeec06c8899f35a46393956a2989904a
ysun09 <ysun@brynmawr.edu>
2021-04-28 17:07:10 +0800

    Adding dashboard queries

--- server/routes.js
+++ server/routes.js
@@ -34,7 +34,57 @@ const getTopStartups = (req, res) => {
   });
 };
 
+const getRecentInvestments = (req, res) => {
+  const query = `
+WITH c_num AS (
+SELECT COUNT(*) AS number
+FROM CompanyInvestIn
+WHERE date LIKE '%2013-%'
+)
+SELECT COUNT(*)+c_num.number AS number
+FROM PersonInvestIn, c_num 
+WHERE date LIKE '%2013-%';
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
 
+const getFoundersSchools = (req, res) => {
+  const query = `
+WITH total_fund AS (
+	SELECT a.p_id, d.institution, SUM(i.amount)/COUNT(*) AS p_amount
+	FROM Degree d JOIN Affiliates a ON d.receiver = a.p_id JOIN InvestIn i ON a.c_id = i.c_id
+	WHERE a.title LIKE '%Founder%'
+	GROUP BY a.p_id
+)
+	SELECT institution, SUM(p_amount) AS i_amount
+	FROM total_fund
+	GROUP BY institution
+	ORDER BY i_amount DESC;
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
+
+const getInvestmentYears = (req, res) => {
+  const query = `
+  SELECT SUM(i.amount) FROM InvestsIn i GROUP BY YEAR(i.date);
+  `;
+  connection.query(query, function(err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+};
 /* ---- Select Queries ---- */
 
 const selectYearVC = (req, res) => {
@@ -178,6 +228,9 @@ const getIndustryStartup = (req, res) => {
 module.exports = {
 	getTopInvestors: getTopInvestors,
   getTopStartups: getTopStartups,
+  getRecentInvestments: getRecentInvestments,
+  getFoundersSchools: getFoundersSchools,
+  getInvestmentYears: getInvestmentYears,
   selectYearVC: selectYearVC,
   selectYearStartup: selectYearStartup,
   selectYearIndustry: selectYearIndustry,
