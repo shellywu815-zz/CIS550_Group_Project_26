04c03fd8185ef0e022d66ddd1a49c6d6555cc2dc
ysun09 <ysun@brynmawr.edu>
2021-05-08 23:28:49 +0800

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -47,14 +47,10 @@ const getTopStartups = (req, res) => {
 
 const getRecentInvestments = (req, res) => {
   const query = `
-WITH c_num AS (
-SELECT COUNT(*) AS number
-FROM CompanyInvestIn
-WHERE date LIKE '%2013-%'
-)
-SELECT COUNT(*)+c_num.number AS number
-FROM PersonInvestIn, c_num 
-WHERE date LIKE '%2013-%';
+  SELECT COUNT(*) AS number, YEAR(date) as year
+  FROM CompanyInvestIn
+  GROUP BY YEAR(date)
+  ORDER BY YEAR(date) DESC;
   `;
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
@@ -68,14 +64,15 @@ const getFoundersSchools = (req, res) => {
   const query = `
 WITH total_fund AS (
 	SELECT a.p_id, d.institution, SUM(i.amount)/COUNT(*) AS p_amount
-	FROM Degree d JOIN Affiliates a ON d.receiver = a.p_id JOIN InvestIn i ON a.c_id = i.c_id
+	FROM Degree d JOIN Affiliates a ON d.receiver = a.p_id JOIN CompanyInvestIn i ON a.c_id = i.invested_id
 	WHERE a.title LIKE '%Founder%'
 	GROUP BY a.p_id
 )
 	SELECT institution, SUM(p_amount) AS i_amount
 	FROM total_fund
 	GROUP BY institution
-	ORDER BY i_amount DESC;
+	ORDER BY i_amount DESC
+	LIMIT 30;
   `;
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
@@ -87,9 +84,10 @@ WITH total_fund AS (
 
 const getInvestmentYears = (req, res) => {
   const query = `
-  SELECT YEAR(i.date), SUM(i.amount)
-  FROM InvestsIn i
-  GROUP BY YEAR(i.date);
+  SELECT YEAR(i.date) AS year, SUM(i.amount) AS amount
+  FROM CompanyInvestIn i
+  GROUP BY YEAR(i.date)
+  ORDER BY YEAR(i.date) DESC;
   `;
   connection.query(query, function(err, rows, fields) {
     if (err) console.log(err);
